e0431602f0893120bca803ccc6f0a85093f01542
changeforan <changeforan@gmail.com>
2018-10-08 16:20:34 +0800

    Update fx.py

--- fx.py
+++ fx.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-class Quadratic:
+
 # f(x) = ax^2+b
+class Quadratic:
     def __init__(self,
                  a,
                  b,
@@ -12,9 +13,9 @@ class Quadratic:
         self.y_ = np.square(self.x_) * a + b
 
 
-class Recursion:
 # a_0 = 0
 # a_(n+1) = {a_(n)^2 + 1} / 2
+class Recursion:
     def __init__(self, num):
         self.a = None
         self.b = 0
@@ -22,7 +23,7 @@ class Recursion:
         self.x_ = np.linspace(0, num - 1, num)[:, np.newaxis]
         self.y_ = list(self)
 
-    def next(self):
+    def __next__(self):
         self.num -= 1
         if self.num >= 0:
             self.a, self.b = self.b, (self.b ** 2 + 1.) / 2
@@ -32,3 +33,28 @@ class Recursion:
 
     def __iter__(self):
         return self
+
+
+# Pi(x)
+# The number of primes not exceeding x
+class PiFunction:
+    def __init__(self, x):
+        self.x_ = np.linspace(0, x, x + 1)[:, np.newaxis]
+        self.y_ = pi_arr(x)
+
+
+def pi_arr(n):
+    arr = [i for i in range(n + 1)][2:]
+    for i in range(int(n ** 0.5) + 1):
+        if arr[i] > 0:
+            j = 2
+            while j * arr[i] <= n:
+                arr[j * arr[i] - 2] = 0
+                j += 1
+    pi = [[0], [0]]
+    count = 0
+    for i in range(0, n - 1):
+        if arr[i] > 0:
+            count += 1
+        pi.append([count / 1.])
+    return pi
