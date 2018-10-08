77aafab661f713a8751649c7c30f9e0dd77c582f
changeforan <changeforan@gmail.com>
2018-10-08 16:20:01 +0800

    Update train.py

--- train.py
+++ train.py
@@ -4,58 +4,55 @@ import datainput
 import fx
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
-    # add one more layer and return the output of this layer
-    weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    Wx_plus_b = tf.matmul(inputs, weights) + biases
-    if activation_function is None:
-        outputs = Wx_plus_b
-    else:
-        outputs = activation_function(Wx_plus_b)
-    return outputs
+def network(x):
+    fc1 = fc_layer(x, 10, "fc1")
+    ac1 = tf.nn.relu(fc1)
+    fc2 = fc_layer(ac1, 20, "fc2")
+    ac2 = tf.nn.sigmoid(fc2)
+    fc3 = fc_layer(ac2, 20, "fc3")
+    ac3 = tf.nn.sigmoid(fc3)
+    fc4 = fc_layer(ac3, 1, "fc4")
+    return fc4
+
+
+def fc_layer(bottom, n_weight, name):
+    assert len(bottom.get_shape()) == 2
+    n_prev_weight = bottom.get_shape()[1]
+    W = tf.get_variable(name+'W',
+                        dtype=tf.float32,
+                        shape=[n_prev_weight, n_weight],
+                        initializer=tf.truncated_normal_initializer(stddev=0.5))
+    b = tf.get_variable(name+'b',
+                        dtype=tf.float32,
+                        initializer=tf.constant(0.1,
+                                                shape=[n_weight],
+                                                dtype=tf.float32))
+    fc = tf.nn.bias_add(tf.matmul(bottom, W), b)
+    return fc
 
 
 # define placeholder for inputs to network
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
-# add hidden layer
-l1 = add_layer(xs, 1, 100, activation_function=tf.nn.sigmoid)
 
-l4 = add_layer(l1, 100, 10, activation_function=tf.nn.sigmoid)
-# add output layer
-prediction = add_layer(l4, 10, 1, activation_function=None)
+prediction = network(xs)
 
 # the error between prediction and real data
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
-                                    reduction_indices=[1]))
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
-# important step
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
-
-
-
-data = datainput.DataInput(
-    fx.Recursion(100)
-)
-
-
-
-
-# plot the real data
+data = datainput.DataInput(fx.PiFunction(100))
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(16000):
+        x, y = data.next_batch(20)
+        sess.run(train_step, feed_dict={xs: x, ys: y})
+    prediction_value = sess.run(prediction, feed_dict={xs: data.x_})
+    lines = ax.plot(data.x_, prediction_value, 'ro', lw=3)
 
-for i in range(4000):
-    x, y = data.next_batch(20)
-    sess.run(train_step, feed_dict={xs: x, ys: y})
 
-
-prediction_value = sess.run(prediction, feed_dict={xs: data.x_})
-lines = ax.plot(data.x_, prediction_value, 'ro', lw=3)
 ax.scatter(data.x_, data.y_)
 plt.show()
